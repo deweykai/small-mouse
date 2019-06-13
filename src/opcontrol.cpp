e50a2cdd5eec95e641b94533a3058eeccbf74182
KAIS41 <KAIS41@KAIS41s-MacBook-Pro.local>
2019-06-13 16:38:56 +0900

    refactored some code

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -17,9 +17,11 @@
 using namespace okapi;
 
 // motor ports
-const int DRIVE_MOTOR_LEFT = 2;
-const int DRIVE_MOTOR_RIGHT = 1;
-const int LIFT_MOTOR = 3;
+const Motor DRIVE_MOTOR_RIGHT = 1_rmtr;
+const Motor DRIVE_MOTOR_LEFT = 2_mtr;
+const Motor LIFT_MOTOR_RIGHT = 3_mtr;
+const Motor LIFT_MOTOR_LEFT = 4_rmtr;
+const MotorGroup LIFT_MOTOR_GROUP({LIFT_MOTOR_LEFT, LIFT_MOTOR_RIGHT});
 
 // dimensions used for tracking position
 const auto WHEEL_DIAMETER = 10_cm;
@@ -32,15 +34,16 @@ auto drive = ChassisControllerFactory::create(
 );
 
 // control of lift motors async from main loop
-auto liftControl = AsyncControllerFactory::posIntegrated(LIFT_MOTOR);
+auto liftControl = AsyncControllerFactory::posIntegrated(LIFT_MOTOR_GROUP);
 
 // predefined heights for lift
 const int NUM_HEIGHTS = 4;
 const int height1 = 0;
 const int height2 = 20;
 const int height3 = 40;
-const int height4 = 60;
+const int height4 = 90;
 const int heights[NUM_HEIGHTS] = {height1, height2, height3, height4};
+const int armGearRatio = 7;
 
 // buttons for controlling lift
 ControllerButton btnUp(ControllerDigital::R1);
@@ -54,11 +57,19 @@ void opcontrol() {
 	// value to track the current goal height
 	int goalHeight = 0;
 
+	// set max angular velocity for arms
+//	int maxAngularVelocity = 1;
+//	leftLiftControl.setMaxVelocity(maxAngularVelocity);
+//	rightLiftControl.setMaxVelocity(maxAngularVelocity);
+
 	while (true) {
 		// print to lcd screen
 		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
 		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+
+		controller.clearLine(1);
+		controller.setText(1, 0, "Hello");
 
 		// drive tank controls
 		drive.tank(controller.getAnalog(ControllerAnalog::leftY),
@@ -68,10 +79,10 @@ void opcontrol() {
 		// set the target height
 		if (btnUp.changedToPressed() && goalHeight < NUM_HEIGHTS - 1) {
 			goalHeight++;
-			liftControl.setTarget(heights[goalHeight]);
+			liftControl.setTarget(heights[goalHeight] * armGearRatio);
 		} else if (btnDown.changedToPressed() && goalHeight > 0) {
 			goalHeight--;
-			liftControl.setTarget(heights[NUM_HEIGHTS]);
+			liftControl.setTarget(heights[goalHeight] * armGearRatio);
 		}
 
 		pros::delay(20);
