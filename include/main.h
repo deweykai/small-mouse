605ef335f17860592fc3148973b342cd8cd33d60
KAIS41 <KAIS41@KAIS41s-MacBook-Pro.local>
2019-11-20 18:23:39 +0900

    updated prosv5 software

--- include/main.h
+++ include/main.h
@@ -4,7 +4,7 @@
  * Contains common definitions and header files used throughout your PROS
  * project.
  *
- * Copyright (c) 2017-2018, Purdue University ACM SIGBots.
+ * Copyright (c) 2017-2019, Purdue University ACM SIGBots.
  * All rights reserved.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
@@ -39,7 +39,7 @@
 /**
  * You should add more #includes here
  */
-#include "okapi/api.hpp"
+//#include "okapi/api.hpp"
 //#include "pros/api_legacy.h"
 
 /**
